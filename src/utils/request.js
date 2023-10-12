7355c8a2b36802d1191c8795e5359e5e3cac6438
zrpppp <popoasd@foxmail.com>
2023-10-12 11:04:00 +0800

    添加云端接口

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
   // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  baseURL: 'http://127.0.0.1:4523/m1/3202416-0-default',
+  baseURL: 'https://mock.apifox.cn/m1/3405279-0-default',
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
